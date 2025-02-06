3f0e8b4ad046415e74a42261b8ef9025c44be3fb
kohei.matsui <kohei.matsui@DOS-PC-1298.dos01-osaka.local>
2025-02-07 08:55:46 +0900

    2025022070900

---
+++ main/Pokepoke/src/main/java/com/example/demo/controller/ZukanController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.repository.ZukanRepository;
+
+@Controller
+@RequestMapping("/zukan")
+public class ZukanController {
+    @Autowired
+    private ZukanRepository zukanRepository;
+    //private ZukanRepository zukanRepository = new ZukanRepository();
+    @GetMapping
+    public String listZukans(Model model) {
+    	
+        //model.addAttribute("zukan", zukanRepository.findAllByOrderByIdAsc());
+        model.addAttribute("items", zukanRepository.findAll());
+        
+        //System.out.println(model);
+        //return "zukan/zukan.html";
+        return "zukans";
+    }
+
+}
---
+++ main/Pokepoke/src/main/java/com/example/demo/repository/ZukanRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Zukan;
+
+
+public interface ZukanRepository extends JpaRepository<Zukan, Integer> {
+	//@Query("SELECT t FROM zukan t ORDER BY t.id ASC")
+    //List<Zukan> findAllByOrderByIdAsc();
+	
+}
