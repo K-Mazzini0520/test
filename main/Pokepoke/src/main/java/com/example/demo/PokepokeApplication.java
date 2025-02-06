6aff18c21b11db9df7fe40e1021f19197d5c871b
kohei.matsui <kohei.matsui@DOS-PC-1298.dos01-osaka.local>
2025-02-06 17:27:17 +0900

    図鑑から呼ぶのできそう

--- main/Pokepoke/src/main/java/com/example/demo/PokepokeApplication.java
+++ main/Pokepoke/src/main/java/com/example/demo/PokepokeApplication.java
@@ -2,9 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 //(exclude = {DataSourceAutoConfiguration.class })
+@EntityScan("com.example.demo.model")//←今回追加したアノテーション
+@EnableJpaRepositories("com.example.demo.repository")
 public class PokepokeApplication {
     public static void main(String[] args) {
         SpringApplication.run(PokepokeApplication.class, args);
